07a6412cbd7f18257470d5979b539204aae28a1e
Ladislav Slezák <lslezak@suse.com>
2023-11-04 22:45:47 +0100

    Indicate the active language

--- src/components/LangButtons.js
+++ src/components/LangButtons.js
@@ -3,6 +3,10 @@ import React from "react";
 import { Button } from "@patternfly/react-core";
 import { useTranslation } from "react-i18next";
 
+import i18next from "i18next";
+
+import "./LangButton.css";
+
 export default function LangButtons() {
   const { i18n } = useTranslation("texts");
 
@@ -11,6 +15,7 @@ export default function LangButtons() {
       <Button
         isInline
         variant="link"
+        className={i18next.language === "en" && "activeLanguage"}
         onClick={() => i18n.changeLanguage("en")}
       >
         English
@@ -19,6 +24,7 @@ export default function LangButtons() {
       <Button
         isInline
         variant="link"
+        className={i18next.language === "cs" && "activeLanguage"}
         onClick={() => i18n.changeLanguage("cs")}
       >
         Čeština
